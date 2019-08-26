58261b393e87364a1d5307a4205cd641538be3a9
Vincent Slifer <Vincent.Slifer@gmail.com>
2019-08-26 11:26:53 +0000

    Added axios, added method to retrieve notes from back-end API

--- src/components/List.js
+++ src/components/List.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 
 class List extends Component {
+
+	componentWillMount() {
+		this.props.getNotes();
+	}
+
 	render() {
 		return (<div className = "list-container" >
 			List Component
