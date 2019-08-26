7c535ae8133223d04f6c85cb30fd8267465009e4
Vincent Slifer <Vincent.Slifer@gmail.com>
2019-08-26 06:25:02 +0000

    Created list and note components

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 class Nav extends React.Component {
 	render() {
-		return (<div>
-			Nav Component
+		return (<div className="nav-container">
+			<div className="nav-logo">Note</div>
+			<div className="nav-button">+ Note</div>
 		</div>);
 	}
 }
